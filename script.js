395f49804e84962c974c96ba51a552ea9be6e166
mohamedeltahan1470 <mohamedeltahan1470@gmail.com>
2024-06-03 17:07:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+openPopup;
+
+function closePopup(){
+    popup.classList.remove("open-popup")
+}
+closePopup;
